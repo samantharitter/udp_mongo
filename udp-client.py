230bbf090bb9414fd0068b2bea2d98f5630588c3
Samantha Ritter <samantha.ritter@10gen.com>
2015-03-06 00:10:35 -0500

    Add time trials

--- udp-client.py
+++ udp-client.py
@@ -1,8 +1,10 @@
 import bson
 import pymongo
 import socket
+import string
 import struct
 import random
+import time
 
 from bson.objectid import ObjectId
 from bson.son import SON
@@ -52,7 +54,6 @@ def format_wp_insert(doc):
     data.write(encoded)
 
     request_id, final_message = _insert_message(data.getvalue())
-    print "sending this data " + final_message
     return final_message
 
 def doc_from_message(message):
@@ -61,17 +62,101 @@ def doc_from_message(message):
     doc['message'] = message
     return doc
 
-def send_over_udp(data):
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    addr = ("localhost", 26000)
-    sock.sendto(data, addr)
-
-def send(message, port):
+def send_over_udp_socket(message, sock, addr):
     doc = doc_from_message(message)
-    wp = format_wp_insert(doc)
-    if not wp:
+    data = format_wp_insert(doc)
+    if not data:
         print "boo there was a problem"
         return
-    send_over_udp(wp)
+    sock.sendto(data, addr)
 
-send("hello python", 26000)
+def send_over_udp(message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = ("localhost", 26000)
+    send_over_udp_socket(message, sock, addr)
+    sock.close()
+
+def send_over_tcp_socket(message, sock):
+    doc = doc_from_message(message)
+    data = format_wp_insert(doc)
+    if not data:
+        print "boo there was a problem"
+        return
+    sock.send(data)
+
+def send_over_tcp(message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("localhost", 27017))
+    send_over_tcp_socket(message, sock)
+    sock.close()
+
+def send(message):
+    send_over_tcp(message)
+
+def random_string(n):
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(n))
+
+
+def time_trial_tcp_separate_connections(n):
+    for x in range(0, n):
+        msg = random_string(24)
+        send_over_tcp(msg)
+    return
+
+def time_trial_tcp_one_connection(n):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("localhost", 27017))
+
+    for x in range(0, n):
+        msg = random_string(24)
+        send_over_tcp_socket(msg, sock)
+
+    sock.close()
+    return
+
+def time_trial_udp_one_socket(n):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = ("localhost", 26000)
+
+    for x in range(0, n):
+        msg = random_string(24)
+        send_over_udp_socket(msg, sock, addr)
+
+    sock.close
+
+def time_trial_udp_separate_sockets(n):
+    for x in range(0, n):
+        msg = random_string(24)
+        send_over_udp(msg)
+    return
+
+def run_time_trial(n):
+    ## how long does it take each implementation to send n documents?
+    print "\none connection/socket:\n"
+
+    start = time.clock()
+    time_trial_tcp_one_connection(n)
+    print "\tTCP:\t", time.clock() - start, " seconds"
+
+    start = time.clock()
+    time_trial_udp_one_socket(n)
+    print "\tUDP:\t", time.clock() - start, " seconds"
+
+    print "\nseparate connections/sockets:\n"
+
+    start = time.clock()
+    time_trial_tcp_separate_connections(n)
+    print "\tTCP:\t", time.clock() - start, " seconds"
+
+    start = time.clock()
+    time_trial_udp_separate_sockets(n)
+    print "\tUDP:\t", time.clock() - start, " seconds\n"
+
+print "========== 100 inserts =========="
+run_time_trial(100)
+print "========== 1000 inserts =========="
+run_time_trial(1000)
+print "========== 10000 inserts =========="
+run_time_trial(10000)
+print "========== 100000 inserts =========="
+run_time_trial(100000)
